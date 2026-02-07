f8ef55a5e0910caf86699064958cb7f2e49fa946
AleyJan <aliriayat507@gmail.com>
2026-02-07 10:17:18 +0500

    update in protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
+export default function ProtectedRoute() {
   const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/login" />;
+
+  return token ? <Outlet /> : <Navigate to="/login" />;
 }
